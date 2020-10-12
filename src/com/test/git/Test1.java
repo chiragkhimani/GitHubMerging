4372d5a5eaf2e7359a6ff1359d14a6a177ad93b9
khimanichirag <khimanichirag@gmail.com>
2020-10-12 18:07:40 +0530

    same file

--- src/com/test/git/Test1.java
+++ src/com/test/git/Test1.java
@@ -2,7 +2,7 @@ package com.test.git;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("First line");
+		System.out.println("Initial line");
 		System.out.println("second line");
 	}
 }
